85cdca11c57e741b7f48850895d2c77264eb60c3
Eric <yellastar42@yahoo.com>
2021-04-26 01:39:22 -0500

    heroku initial

--- server/server.js
+++ server/server.js
@@ -1,14 +1,13 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const { Schema } = mongoose
-const PORT = 5000
-
+const PORT = process.env.PORT || 5000
 
 const app = express()
 
 app.use(express.json())
 
-const DB = 'mongodb://localhost:27017/Groceries'
+const DB = process.env.MONGO || 'mongodb+srv://ericb:abcd1234@cluster0.fvto2.mongodb.net/Groceries'
 
 mongoose
 .connect(DB, { useNewUrlParser: true }, { useUnifiedTopology: true })
@@ -20,22 +19,51 @@ const itemSchema = new Schema({
     quantity: Number
 })
 
-mongoose.model('items', itemSchema)
-
-
+const items = mongoose.model('items', itemSchema)
 
 app.get('/items', async (req,res) => {
-    const result = await mongoose.model('items').find({})
+    const result = await items.find({})
+    res.json(result)
+})
+
+app.get('/items/:id', async (req,res) => {
+    const {id} = req.params
+    const result = await items.find({_id:id})
     res.json(result)
 })
 
 app.post('/items', async (req,res) => {
     const {name, quantity } = req.body
-    const newItem = {
-        name,
-        quantity
+    const newItem = new items({name, quantity})
+
+    const data = await newItem.save() 
+    res.json(data)
+})
+
+app.put('/items/:id', async (req,res) => {
+    const {id} = req.params
+    const {name, quantity } = req.body
+    if(name || quantity){
+    await items.updateOne({_id:id}, {...req.body})
+        .catch((err) => {
+            console.log(err) 
+            res.json({error:"not updated"})
+        })
+    res.json({item:"updated"})
     }
-    await mongoose.model('items').save(newItem).then((data)=> res.json(data))
+    else res.json({error:"No Change"})
+})
+
+app.delete('/items/:id', async (req,res) => {
+    const { id } = req.params
+        if(id){
+            const removed = await items.deleteOne({ _id: id }).catch(err => {
+                console.log(err)
+                res.json({error:"item not deleted"})
+        })
+        return res.json({deleted:"Item removed"})
+    }
+    res.json({error:"Item not found"})
 })
 
 app.listen(PORT, (err) =>{
